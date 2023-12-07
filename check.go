2cb974bf367d71414ba285be96b0d2aeb95b2874
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-12-07 17:08:21 +0100

    Check function defined in status.go to return status

--- check.go
+++ check.go
@@ -1,5 +1,21 @@
 package main
 
 import (
-	
+	"fmt"
+	"net"
+	"time"
 )
+
+func Check(destination string, port string) string {
+	address := destination + ":" + port
+	timeout := time.Duration(5 * time.Second)
+	conn, err := net.DialTimeout("tcp", address, timeout)
+
+	var status string
+	if err != nil {
+		status = fmt.Sprintf("[DOWN] %v is reachable, Error : %v", destination, err)
+	} else {
+		status = fmt.Sprintf("[UP] %v is reachable, Form: %v to: %v", destination, conn.LocalAddr(), conn.RemoteAddr())
+	}
+	return status
+}
